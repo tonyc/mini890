8d82263436cf1c3f22e7711f0ecae61d5c7bb6c2
Tony Collen <tony@halogenlabs.com>
2021-12-12 15:42:10 -0600

    Better logging for the server

--- server.rb
+++ server.rb
@@ -1,49 +1,55 @@
+#!/usr/bin/env ruby
+
 require 'socket'
 PORT = 1234
+
+def log(str)
+  puts "[#{Time.now}] #{str}"
+end
 
 def handle_line(line, socket)
   case line.chomp(";")
   when "##CN"
-    puts "Received connection request, allowing connections"
+    log "Received connection request, allowing connections"
     socket.write "##CN1;"
   when "##ID10705kenwoodadmin"
-    puts "Successfully authenticated user"
+    log "Successfully authenticated user"
     client_authenticated = true
     socket.write "##ID1"
   when /^##ID/
-    puts "Incorrect authentication, rejecting: #{line}"
+    log "Incorrect authentication, rejecting: #{line}"
     socket.write "##ID0;"
   when "AI1"
-    puts "Auto-info ON"
+    log "Auto-info ON"
     socket.write line
   when "PS"
     socket.write "PS1;"
   else
-    puts "Received unknown command: #{line.inspect}"
+    log "Received unknown command: #{line.inspect}"
     socket.write "?;"
   end
 
   socket.flush()
 end
 
-puts "starting tcp server on :#{PORT}"
+log "starting tcp server on :#{PORT}"
 
 TCPServer.open(PORT) do |server|
-  puts "**** Server waiting for connections"
+  log "**** Server waiting for connections"
 
   loop do
     socket = server.accept
 
-    puts "*** new client"
-    puts "Accepted connection on socket: #{socket.inspect}"
+    log "*** new client"
+    log "Accepted connection on socket: #{socket.inspect}"
 
     client_authenticated = false
 
     while line = socket.gets(";") do
-      puts "socket.gets raw line: #{line.inspect}"
+      log "socket.gets raw line: #{line.inspect}"
 
       if !line
-        puts "Read nil line, sleeping"
+        log "Read nil line, sleeping"
         sleep 2
         next
       end
@@ -51,7 +57,7 @@ TCPServer.open(PORT) do |server|
       handle_line(line, socket)
     end
 
-    puts "End of socket read loop"
+    log "End of socket read loop"
     socket.close
   end
 
