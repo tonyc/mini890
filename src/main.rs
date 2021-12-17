fffae720c51dbf694a4b60fa96505e6caea2ee7c
Tony Collen <tony@halogenlabs.com>
2021-12-17 09:55:56 -0600

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,54 +9,51 @@ fn main() {
     static CMD_REQUEST_CONNECTION: &str = "##CN;";
     static CMD_USER_PASS: &str = "##ID10705kenwoodadmin;";
 
-    match TcpStream::connect("localhost:1234") {
-        Ok(mut stream) => {
-            println!("Connected to server on port 1234");
+    let mut stream: TcpStream = TcpStream::connect("localhost:1234").expect("Could not connect to server");
 
-            stream.write(CMD_REQUEST_CONNECTION.as_bytes()).unwrap();
-            println!("Sent connection request CN, awaiting reply...");
+    println!("Connected to server on port 1234");
 
-            let mut data = [0 as u8; 1024];
+    stream.write(CMD_REQUEST_CONNECTION.as_bytes()).unwrap();
+    println!("Sent connection request CN, awaiting reply...");
 
-            stream.read(&mut data).unwrap();
-            let text = from_utf8(&data).unwrap();
+    let mut data = [0 as u8; 1024];
 
-            // BUG: text has the entire 1k buffer padded with zeroes
-            println!("Read text: {}", text);
+    stream.read(&mut data).unwrap();
 
-            let response = read_cmd(text);
+    let text = from_utf8(&data).unwrap();
 
-            if RESP_CONNECTION_ALLOWED.eq(response) {
-                println!("Sending username/password");
-                stream.write(CMD_USER_PASS.as_bytes()).unwrap();
+    // BUG: text has the entire 1k buffer padded with zeroes
+    println!("Read text: {}", text);
 
-                // BUG: We should probably reset the data buffer each time we read
-                stream.read(&mut data).unwrap();
+    let response = read_cmd(text);
 
-                let text = from_utf8(&data).unwrap();
-                println!("Authentication response: {}", text);
+    if RESP_CONNECTION_ALLOWED.eq(response) {
+        println!("Sending username/password");
+        stream.write(CMD_USER_PASS.as_bytes()).unwrap();
 
-                let response = read_cmd(text);
+        // BUG: We should probably reset the data buffer each time we read
+        stream.read(&mut data).unwrap();
 
-                if RESP_AUTHENTICATION_SUCCESSFUL.eq(response) {
-                    println!("Successfully authenticated!");
-                } else {
-                    println!("Incorrect username/password");
-                }
-            } else {
-                println!("Connection denied");
-            }
+        let text = from_utf8(&data).unwrap();
+        println!("Authentication response: {}", text);
+
+        let response = read_cmd(text);
+
+        if RESP_AUTHENTICATION_SUCCESSFUL.eq(response) {
+            println!("Successfully authenticated!");
+        } else {
+            println!("Incorrect username/password");
         }
-        Err(e) => {
-            println!("Failed to connect: {}", e);
-        }
+    } else {
+        println!("Connection denied");
     }
+
     println!("Client Terminated.");
 }
 
 // slices a str up to the first semicolon
 fn read_cmd(s: &str) -> &str {
-    let pos = s.find(";").unwrap();
+    let pos: usize = s.find(";").unwrap();
     &s[..pos]
 }
 
