0485c3df4bf008261a945c9f358718c5adde402e
Tony Collen <tony@halogenlabs.com>
2022-02-26 10:46:14 -0600

    Rework a bunch of stuff, dynamically generate the login sequence, use Result's

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,36 @@ use std::net::TcpStream;
 use std::str::from_utf8;
 // use std::vec;
 
+const RESP_AUTHENTICATION_SUCCESSFUL: &str = "##ID1";
+const RESP_CONNECTION_ALLOWED: &str = "##CN1";
+const CMD_REQUEST_CONNECTION: &str = "##CN;";
+
+//const CMD_USER_PASS: &str = "##ID10705kenwoodadmin;";
+const HOST: &str = "192.168.1.229:60000";
+const USER: &str = "testuser";
+const PASS: &str  = "testpass123!";
+const BUFFER_SIZE:usize = 1286; // this appears to be the maximum size of the ##DD2 bandscope message
+
 fn main() {
-    const RESP_AUTHENTICATION_SUCCESSFUL: &str = "##ID1";
-    const RESP_CONNECTION_ALLOWED: &str = "##CN1";
-
-    const CMD_REQUEST_CONNECTION: &str = "##CN;";
-    const CMD_USER_PASS: &str = "##ID10705kenwoodadmin;";
-
-    const BUFFER_SIZE:usize = 1286;
-
-    let mut stream: TcpStream = TcpStream::connect("localhost:1234").expect("Could not connect to server");
+    let stream: TcpStream = TcpStream::connect(HOST).expect("Could not connect to server");
 
     println!("Connected to server on port 1234");
+
+    match authenticate(&stream) {
+        Ok(_stream) => {
+            println!("Authentication successful!")
+        }
+
+        Err(msg) => {
+            println!("Error authenticating: {}", msg)
+        }
+    }
+
+    println!("Client Terminated.");
+}
+
+fn authenticate(mut stream: &TcpStream) -> Result<&TcpStream, &str> {
+    let cmd_user_pass: String = make_login(&USER, &PASS);
 
     stream.write(CMD_REQUEST_CONNECTION.as_bytes()).unwrap();
     println!("Sent connection request CN, awaiting reply...");
@@ -29,10 +47,10 @@ fn main() {
     // BUG: text has the entire 1k buffer padded with zeroes
     println!("Read text: {}", text);
 
-    match find_cmd(text) {
+    let res = match find_cmd(text) {
         RESP_CONNECTION_ALLOWED => {
             println!("Sending username/password");
-            stream.write(CMD_USER_PASS.as_bytes()).unwrap();
+            stream.write(cmd_user_pass.as_bytes()).unwrap();
 
             // BUG: We should probably reset the data buffer each time we read
             stream.read(&mut buf).unwrap();
@@ -43,22 +61,30 @@ fn main() {
             let response = find_cmd(text);
 
             if RESP_AUTHENTICATION_SUCCESSFUL.eq(response) {
-                println!("Successfully authenticated!");
+                Ok(stream)
             } else {
-                println!("Incorrect username/password");
+                Err("Incorrect username/password")
             }
 
         }
 
-        _ => { println!("Connection denied"); }
-    }
-
-    println!("Client Terminated.");
+        _ => {
+            Err("Connection denied")
+        }
+    };
+    res
 }
 
 // slices a str up to the first semicolon
 fn find_cmd(s: &str) -> &str {
     let pos: usize = s.find(";").unwrap();
     &s[..pos]
+}
+
+fn make_login(user: &str, pass: &str) -> String {
+    let user_len = format!("{:0>2}", user.len());
+    let pass_len = format!("{:0>2}", pass.len());
+
+    format!("##ID1{}{}{}{};", user_len, pass_len, user, pass)
 }
 
