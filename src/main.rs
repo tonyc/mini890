c346c0271aa234f95f60bbee96753909ecdd15e7
Tony Collen <tony@halogenlabs.com>
2022-02-26 21:25:45 -0600

    more general hacking, turn it into a pair of threads

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
-// use std::vec;
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
 
 const RESP_AUTHENTICATION_SUCCESSFUL: &str = "##ID1";
 const RESP_CONNECTION_ALLOWED: &str = "##CN1";
@@ -10,31 +12,85 @@ const CMD_REQUEST_CONNECTION: &str = "##CN;";
 //const CMD_USER_PASS: &str = "##ID10705kenwoodadmin;";
 const HOST: &str = "192.168.1.229:60000";
 const USER: &str = "testuser";
-const PASS: &str  = "testpass123!";
-const BUFFER_SIZE:usize = 1286; // this appears to be the maximum size of the ##DD2 bandscope message
+const PASS: &str = "testpass123!";
+const BUFFER_SIZE: usize = 1286; // this appears to be the maximum size of the ##DD2 bandscope message
 
 fn main() {
-    let stream: TcpStream = TcpStream::connect(HOST).expect("Could not connect to server");
+    let mut stream: TcpStream = TcpStream::connect(HOST).expect("Could not connect to server");
 
     println!("Connected to server on port 1234");
 
-    match authenticate(&stream) {
-        Ok(_stream) => {
-            println!("Authentication successful!")
-        }
+    radio_authenticate(&stream).expect("Could not authenticate");
+    println!("Authentication successful!");
 
-        Err(msg) => {
-            println!("Error authenticating: {}", msg)
-        }
-    }
+    let (tx, rx) = mpsc::channel();
 
-    println!("Client Terminated.");
+    let timer_thread = thread::spawn(move || {
+        println!("spawning timer thread");
+        loop {
+            println!(" --> async thread!");
+            //send_cmd(&stream, "PS;");
+            tx.send("PS").unwrap();
+            sleep(1000);
+        }
+    });
+
+    let connection_thread = thread::spawn(move || {
+        println!("spawning connection thread");
+
+        send_cmd(&stream, "AI2;").unwrap();
+        //send_cmd(&stream, "DD11;").unwrap();
+        // now just start reading commands in a loop
+        let mut buf = [' ' as u8; BUFFER_SIZE];
+        loop {
+            //let received_command: &str = rx.recv().unwrap();
+            //println!("received: {}", received_command);
+            send_cmd(&stream, "PS;").unwrap();
+            sleep(500);
+
+            match stream.read(&mut buf) {
+                Ok(0) => {
+                    println!("No bytes to read. Did the radio drop the connection?");
+                    break;
+                }
+
+                Ok(n) => {
+                    //println!("read {} bytes", n);
+
+                    //println!("Buffer: {:?}", buf);
+                    let text = from_utf8(&buf[0..n]).unwrap();
+                    //println!("sliced text: {:?}", text);
+
+                    let mut cmds: Vec<&str> = vec![];
+
+                    for cmd in text.split_terminator(";") { //.filter(|x| x.trim() != "") {
+                        cmds.push(cmd);
+                    }
+
+                    for cmd in cmds {
+                        println!("[DN] {}", cmd);
+                    }
+
+                    // reset the buffer
+                    buf.iter_mut().for_each(|x| *x = ' ' as u8);
+                }
+
+                Err(other) => {
+                    println!("Error reading stream: {:?}", other);
+                    break;
+                }
+            }
+        }
+        println!("Client Terminated.");
+
+    });
+
+    timer_thread.join().unwrap();
+    connection_thread.join().unwrap();
 }
 
-fn authenticate(mut stream: &TcpStream) -> Result<&TcpStream, &str> {
-    let cmd_user_pass: String = make_login(&USER, &PASS);
-
-    stream.write(CMD_REQUEST_CONNECTION.as_bytes()).unwrap();
+fn radio_authenticate(mut stream: &TcpStream) -> Result<&TcpStream, &str> {
+    send_cmd(&stream, &CMD_REQUEST_CONNECTION).unwrap();
     println!("Sent connection request CN, awaiting reply...");
 
     let mut buf = [0 as u8; BUFFER_SIZE];
@@ -47,10 +103,10 @@ fn authenticate(mut stream: &TcpStream) -> Result<&TcpStream, &str> {
     // BUG: text has the entire 1k buffer padded with zeroes
     println!("Read text: {}", text);
 
-    let res = match find_cmd(text) {
+    match find_cmd(text) {
         RESP_CONNECTION_ALLOWED => {
             println!("Sending username/password");
-            stream.write(cmd_user_pass.as_bytes()).unwrap();
+            send_cmd(&stream, &login_cmd(USER, PASS)).unwrap();
 
             // BUG: We should probably reset the data buffer each time we read
             stream.read(&mut buf).unwrap();
@@ -58,21 +114,19 @@ fn authenticate(mut stream: &TcpStream) -> Result<&TcpStream, &str> {
             let text = from_utf8(&buf).unwrap();
             println!("Authentication response: {}", text);
 
-            let response = find_cmd(text);
-
-            if RESP_AUTHENTICATION_SUCCESSFUL.eq(response) {
-                Ok(stream)
-            } else {
-                Err("Incorrect username/password")
+            match find_cmd(text) {
+                RESP_AUTHENTICATION_SUCCESSFUL => Ok(stream),
+                _ => Err("Incorrect username/password"),
             }
-
         }
 
-        _ => {
-            Err("Connection denied")
-        }
-    };
-    res
+        _ => Err("Connection denied"),
+    }
+}
+
+fn send_cmd(mut stream: &TcpStream, cmd: &str) -> Result<usize, std::io::Error> {
+    println!("[UP] {}", cmd);
+    stream.write(cmd.as_bytes())
 }
 
 // slices a str up to the first semicolon
@@ -81,10 +135,13 @@ fn find_cmd(s: &str) -> &str {
     &s[..pos]
 }
 
-fn make_login(user: &str, pass: &str) -> String {
+fn login_cmd(user: &str, pass: &str) -> String {
     let user_len = format!("{:0>2}", user.len());
     let pass_len = format!("{:0>2}", pass.len());
 
     format!("##ID1{}{}{}{};", user_len, pass_len, user, pass)
 }
 
+fn sleep(duration: u64) {
+    thread::sleep(Duration::from_millis(duration));
+}
